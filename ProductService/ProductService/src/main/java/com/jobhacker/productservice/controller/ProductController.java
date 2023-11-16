4861f71a3768695881a8355e8443c7a044dd558d
Amir Ibrahim Elkased <amirelkased8558@gmail.com>
2023-11-16 19:22:13 +0200

    modify endpoint of search about product by name

--- ProductService/ProductService/src/main/java/com/jobhacker/productservice/controller/ProductController.java
+++ ProductService/ProductService/src/main/java/com/jobhacker/productservice/controller/ProductController.java
@@ -35,30 +35,19 @@ public class ProductController {
         return new ResponseEntity<>(productDto, HttpStatus.FOUND);
     }
 
-    @GetMapping(value = {"", "/"})
-    public ResponseEntity<List<ProductDto>> getProducts(@RequestParam(value = "name", required = false) String name) {
+    @GetMapping(value = "/search")
+    public ResponseEntity<List<ProductDto>> getProductsByName(@RequestParam(value = "name", required = true) String name) {
 
-        List<ProductDto> resultList;
-        if (name == null) {
-            // Get all Products
-            resultList = productService.getAllProducts();
-        } else {
-            // Search about products
-            resultList = productService.searchAboutProducts(name);
-        }
+        List<ProductDto> resultList = productService.searchAboutProducts(name);
         return new ResponseEntity<>(resultList, HttpStatus.OK);
     }
 
 
-    /*
-
-    Delete it because causes ambiguatiy.
     @GetMapping(value = {"", "/"})
     public ResponseEntity<List<ProductDto>> getAllProducts() {
         List<ProductDto> productList = productService.getAllProducts();
         return new ResponseEntity<>(productList, HttpStatus.OK);
     }
-    */
 
 
     @PutMapping(value = "/{id}")
